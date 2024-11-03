5216b7a2dd9cb1f488206ea94fc1c2331ffb4f4e
un01s <96022876+un01s@users.noreply.github.com>
2024-11-03 15:25:19 -0700

    use-thread

---
+++ NOTES.md
@@ -0,0 +1,32 @@
+# async in rust
+
+## threads
+
+```
+$ cargo run  
+   Compiling rustcode v0.1.0 (/Users/wb/io/code/rustlang/rustcode)
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.89s
+     Running `target/debug/rustcode`
+start 1
+start 4
+start 2
+start 5
+start 6
+start 7
+start 8
+start 3
+start 9
+start 10
+end 4
+end 1
+end 3
+end 9
+end 2
+end 5
+end 6
+end 7
+end 10
+end 8
+```
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,18 @@
-use std::ascii::escape_default;
-use std::str;
+use std::thread;
+use std::time::Duration;
 
-static BYTES: &[u8] = b"hello world!";
-
-fn show(bs: &[u8]) -> String {
-  let mut visible = String::new();
-  for &b in bs {
-    let part: Vec<u8> = escape_default(b).collect();
-    visible.push_str(str::from_utf8(&part).unwrap());
-  }
-  visible
-}
-
-fn read_one_byte(index: usize) -> u8 {
-  BYTES[index]
-}
-
-/*
-// like returning one byte as *(BYTES+index) in C
-unsafe fn get_one(index: usize) -> u8 {
-  *BYTES.as_ptr().add(index)
-}
-*/
-
-// object soup
-struct Person {
-  name: String,
-  friends: Vec<usize>,
-}
-
-fn new_person(people: &mut Vec<Person>, name: &str) -> usize {
-  people.push(Person { name: name.into(), friends: Vec::new() });
-  people.len() - 1
-}
-
-fn add_friend(people: &mut Vec<Person>, this_id: usize, other_id: usize) {
-  if people[other_id].name != people[this_id].name {
-    people[this_id].friends.push(other_id);
-  }
+fn foo(n: u64) {
+  println!("start {n}");
+  thread::sleep(Duration::from_secs(1));
+  println!("end {n}");
 }
 
 fn main() {
-  let arg = std::env::args()
-        .skip(1)
-        .next()
-        .expect("should have one argument");
-  println!("{}", arg.to_uppercase());
-
-  let string = "onestring";
-  for c in string.chars() {
-    print!("{}", c);
+  let mut thread_handles = Vec::new();
+  for n in 1..=10 {
+    thread_handles.push(thread::spawn(move || foo(n)));
   }
-  println!();
-
-  println!("Hello, world!");
-  println!("{}", read_one_byte(1));
-  //println!("{}", read_one_byte(100));
-
-  // remove unsafe code for object soup
-  //println!("{}", get_one(1));
-
-  let mut people = Vec::new();
-  let alice_id = new_person(&mut people, "Alice");
-  let bob_id = new_person(&mut people, "Bob");
-    
-  add_friend(&mut people, alice_id, bob_id);
-  add_friend(&mut people, bob_id, alice_id);
-  add_friend(&mut people, alice_id, alice_id);
-
-  // use show()
-  let bytes = b"foo\xE2\x98\x83bar\xFFbaz";
-  println!("{}", show(bytes));
-
+  for handle in thread_handles {
+    handle.join().unwrap();
+  }
 }
