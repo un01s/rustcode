597ff3cf8fcc52cc9fcd52ebab136163107aed03
un01s <96022876+un01s@users.noreply.github.com>
2024-11-06 09:21:22 -0700

    guess-game-done

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,28 @@ fn main() {
 
   let secret_num = rand::thread_rng().gen_range(1..=100);
 
-  //println!("Please input your guess.");
+  loop {
+    println!("Please input your guess.");
+    let mut guess = String::new();
 
-  let mut guess = String::new();
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line");
 
-  io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
 
-  let guess: u32 = guess.trim().parse().expect("Please enter a number");
+    println!("You guessed: {guess}");
 
-  println!("You guessed: {}", guess);
-
-  match guess.cmp(&secret_num) {
-    Ordering::Less => println!("Too small!"),
-    Ordering::Greater => println!("Too big!"),
-    Ordering::Equal => println!("You win!"),
+    match guess.cmp(&secret_num) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      }
+    }
   }
-
-  println!("The secret: {}", secret_num);
 }
