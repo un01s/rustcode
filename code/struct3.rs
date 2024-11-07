fe83babffe4f67975da84670b8e9115996552c8b
un01s <96022876+un01s@users.noreply.github.com>
2024-11-07 11:47:13 -0700

    update-struct3

--- code/struct3.rs
+++ code/struct3.rs
@@ -17,6 +17,15 @@ impl Rectangle {
   fn area(&self) -> u32 {
     self.width * self.height
   }
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
+  fn square(size: u32) -> Self {
+    Self {
+      width: size,
+      height: size,
+    }
+  }
 }
 
 // function
@@ -44,5 +53,20 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect.area()
     );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect hold rect2? {}", rect.can_hold(&rect2));
+    println!("Can rect hold rect3? {}", rect.can_hold(&rect3));
+
+    // to call this associated function, we use :: syntax with the struct name
+    let square = Rectangle::square(10);
+    println!("Square area {}", square.area());
 }
 
