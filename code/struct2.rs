73cf4fba348751c33111a4cfbc34cf419b02d141
un01s <96022876+un01s@users.noreply.github.com>
2024-11-07 11:14:41 -0700

    add-struct-example

---
+++ code/struct2.rs
@@ -0,0 +1,54 @@
+/*
+ * rust book
+ * ch5.2 struct
+ *
+ */
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area2(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+
+    // use tuple
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area1(rect1)
+    );
+
+    // use struct to make it better
+    let rect2 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    // now just borrow rect2 to calculate the area
+    // the main function still has the ownership of rect2
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area2(&rect2)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+// use tuple
+fn area1(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
