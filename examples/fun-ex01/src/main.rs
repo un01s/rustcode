6d31eb37700dc331738fb287419c89efe306168f
un01s <96022876+un01s@users.noreply.github.com>
2024-11-22 11:14:45 -0700

    functional

---
+++ examples/fun-ex01/src/main.rs
@@ -0,0 +1,50 @@
+/*
+ * Rust fold
+ * apply a function to each element of the iteration
+ * accumulating the result into a new value
+ *
+ * each iteration of fold takes two arguments.
+ * the first is an initial value for the accumulator.
+ * the second is a closure which itself takes two arguments: 
+ * the accumulator and an item from the iterator.
+ * once the iterator drains, the final value of the accumulator is returned.
+ *
+ */
+
+fn main() {
+  // imperative: what
+  let mut sum = 0;
+  for i in 1..11 {
+    sum += i;
+  }
+  println!("{sum}");
+
+  // declarative: how
+  println!("{}", (1..11).fold(0, |a, b| a + b));
+
+  // another fold example
+  let even_sum = (1..=10).fold(0, |acc, num| 
+    if num%2==0 { 
+      acc + num
+    } else { 
+      acc
+    }); 
+  println!("{even_sum:?}");
+
+  // use filter to do the same
+  //println!("{:?}", (0..11).filter(|n| n%2==0).sum());
+
+  let week_days = vec!["Monday", 
+                        "Tuesday", 
+                        "Wednesday", 
+                        "Thursday", 
+                        "Friday", 
+                        "Saturday", 
+                        "Sunday"];
+  let filtered_week_days:Vec<_> = 
+        week_days.iter()                     
+        .filter(|days| days.len() < 8)
+        .collect();
+  println!("{:?}", filtered_week_days);
+  // ["Monday", "Tuesday", "Friday", "Sunday"]
+}
