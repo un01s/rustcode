e5ec2dafd8bd87f6a4129ab3219c6197f76439e6
un01s <96022876+un01s@users.noreply.github.com>
2024-12-24 11:04:11 -0700

    update-move-borrow

--- study/ex-move/src/main.rs
+++ study/ex-move/src/main.rs
@@ -5,12 +5,27 @@
 //! 
 //! in Rust, we can move or borrow. move is default. to borrow we use &
 //! A borrow might be implemented using a reference, but it might not. 
+//! by default, all bindings in Rust are immutable.
 //!
+//! ownership and mutability interact thusly:
+//! you can have multiple immutable borrows or one mutable borrow.
+//!
+//! Borrows, mutable or not, exist for the lifetime of their scope.
 
 #[derive(Debug)] // easy to print out
 
 struct User {
   id: u32,
+}
+
+impl Clone for User {
+  fn clone(&self) -> Self {
+    User{id: self.id}
+  }
+}
+
+impl Copy for User {
+
 }
 
 // this function takes the ownership away of u
@@ -33,9 +48,9 @@ fn main() {
   let u1 = User{id: 1000};
   println!("{:?}", u1);
 
-  let u2 = u1; // moved, form here u1 has no data
+  //let u2 = u1; // moved, form here u1 has no data
   // assignment or variable binding moves ownership
-  println!("{:?}", u2);
+  // println!("{:?}", u2);
 
   //print_user(u2);
   // now, if you try to print u2, Rust won't and it reports compilation error
@@ -45,4 +60,22 @@ fn main() {
   println!("{:?}", u2);
 
   borrow_ownership(&u2);
+
+  // use an integer instead of struct User type
+  // the integer is a primitive, it has Copy trait.
+  // struct User is a complex and user-created type. 
+  // by default, it has no Copy trait.
+  let i1 = 9001;
+  println!("1st: {}", i1);
+  let i2 = i1; // copied instead of moved
+  println!("2nd: {}", i2);
+  println!("3rd: {}", i1); // this works because the integer has Copy trait
+
+  let u2 = u1; // copied, because of both trait Clone and Copy 
+  println!("u2 copied: {:?}", u2);
+  println!("u1 remains: {:?}", u1);
+
+  let mut a = 1;
+  a += 2;
+  println!("{}", a);
 }
