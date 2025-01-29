5f428cc5c1d7693a9cc59a505da319dcad565c44
un01s <96022876+un01s@users.noreply.github.com>
2025-01-29 15:33:31 -0700

    update-nom

---
+++ examples/ex-nom/README.md
@@ -0,0 +1,10 @@
+# how to use [crate nom](https://crates.io/crates/nom)
+
+nom lets us build simple parsers, and then combine them (using "combinators").
+
+there are two result after the input passes through the parser, Ok or Err.
+
+```
+$ cargo add nom
+```
+
---
+++ examples/ex-nom/src/main.rs
@@ -0,0 +1,60 @@
+use nom::{
+  bytes::complete::{tag, take_while_m_n},
+  combinator::map_res,
+  sequence::Tuple,
+  IResult,
+  Parser,
+};
+
+#[derive(Debug, PartialEq)]
+pub struct Color {
+  pub red: u8,
+  pub green: u8,
+  pub blue: u8,
+}
+
+fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
+  u8::from_str_radix(input, 16)
+}
+
+fn is_hex_digit(c: char) -> bool {
+  c.is_digit(16)
+}
+
+fn hex_primary(input: &str) -> IResult<&str, u8> {
+  map_res(
+    take_while_m_n(2, 2, is_hex_digit),
+    from_hex
+  ).parse(input)
+}
+
+fn hex_color(input: &str) -> IResult<&str, Color> {
+  let (input, _) = tag("#")(input)?;
+  let (input, (red, green, blue)) = (hex_primary, hex_primary, hex_primary).parse(input)?;
+  Ok((input, Color { red, green, blue }))
+}
+
+pub fn do_nothing_parser(input: &str) -> IResult<&str, &str> {
+  Ok((input, ""))
+}
+
+fn main() {
+  println!("{:?}", hex_color("#2F14DF"));
+  println!("{:?}", do_nothing_parser(""));
+}
+
+#[test]
+fn parse_color() {
+  assert_eq!(
+    hex_color("#2F14DF"),
+    Ok((
+      "",
+      Color {
+        red: 47,
+        green: 20,
+        blue: 223,
+      }
+    ))
+  );
+}
+
